5a2f9a0b83aa867d0654fd094ad0a860ef1f2c28
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:35:04 -0500

    first commit

---
+++ src/ThatProduct.java
@@ -0,0 +1,5 @@
+public class ThatProduct {  //That's nice that you named your class ThatProduct, and it's public. How bout that?
+    public static void main(String[] args){ //The beginning of our code, because main.
+        System.out.println( 25 * 5); //Displays the product of two numbers using the Multiplication Operator '*'.
+    }
+}
